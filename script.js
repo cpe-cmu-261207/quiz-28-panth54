ff6157c4be9d3f90b6fe8d87f195c49f0d384ec1
phanthat <phanthat54@gmail.com>
2022-08-04 14:11:00 +0700

    .

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
 }
+
+// document.addEventListener(
+//   "load",
+//   (document.getElementById("div-user-card").style.display = "none")
+// );
+
+const btn = document.getElementById("btn-random");
+btn.onclick = () =>
+  (document.getElementById("div-loading-card").style.display = "none");
